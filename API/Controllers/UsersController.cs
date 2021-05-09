ee95f2b25ec5be06006b891162c0bbfb651b0139
Varga Norbert <varganorbert222@gmail.com>
2021-05-09 22:22:10 +0200

    Continue...

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -29,7 +29,7 @@ namespace API.Controllers
         [HttpGet ("{id}")]
         public async Task<ActionResult<AppUser>> GetUser (int id)
         {
-            return awaitclear _context.Users.FindAsync (id);
+            return await _context.Users.FindAsync (id);
         }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -31,7 +31,7 @@ namespace API
         {
             services.AddDbContext<DataContext>(options =>
            {
-               options.UseSqlite(_config.GetConnectionString ("DefaultConnection"));
+               options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
            });
 
             services.AddControllers();
@@ -39,6 +39,8 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +56,11 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy =>
+            {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+            });
 
             app.UseAuthorization();
 
