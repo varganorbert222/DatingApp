fa9d85ad241bb19c89e2a6837c20bef0c7f2175d
varganorbert222 <varganorbert222@gmail.com>
2021-05-12 14:51:07 +0200

    End of section 8

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDTO>()
+                .ForMember(destination => destination.PhotoUrl, 
+                           options => options.MapFrom(source => source.Photos.FirstOrDefault(photo => photo.IsMain).Url))
+                .ForMember(destination => destination.Age,
+                           options => options.MapFrom(source => source.DateOfBirth.CalculateAge()));
+
+            CreateMap<Photo, PhotoDTO>();
+        }
+    }
+}
